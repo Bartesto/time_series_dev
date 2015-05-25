ef2fdf4b3af74e5a3d3a8dbadbd79f60c9c290a9
Bartesto <barthuntley@iinet.net.au>
2015-05-25 12:25:36 +0800

    added intelligent package install and load

--- mtsd.R
+++ mtsd.R
@@ -18,9 +18,23 @@ project="dhi"
 
 
 mtsd <- function(dir, csv, project) {
-        library(lubridate)
-        library(dplyr)
-        library(zoo)
+        
+        is_installed <- function(mypkg) is.element(mypkg, installed.packages()[,1])
+        
+        load_or_install<-function(package_names)  
+        {  
+                for(package_name in package_names)  
+                {  
+                        if(!is_installed(package_name))  
+                        {  
+                                install.packages(package_name,repos="http://cran.csiro.au/")  
+                        }  
+                        library(package_name,character.only=TRUE,quietly=TRUE,verbose=FALSE)  
+                }  
+        }  
+        
+        load_or_install(c("lubridate","dplyr","zoo"))
+        
         setwd(dir)
         df <- read.csv(csv, header = TRUE)
         df$date <- dmy(df$date)#create date here to order by
